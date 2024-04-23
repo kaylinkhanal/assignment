6896bb532dd0724b9ea511a504a3fb40dcc70fb7
Kaylin Khanal <kyalinnop@gmail.com>
2024-04-23 08:34:41 +0545

    added tasks for operators problem

--- javascript/basic/operators.js
+++ javascript/basic/operators.js
@@ -1,8 +1,11 @@
 //Q1 create two constants, add them assign to a new constants
-
+const x = 10
+const y = 20
+const z = x  + y
+console.log(z)
 
 
 //Check below code, output should be false by comparing. console.log the answer
 const a = 10
 const b = '10'
-
+console.log(a === b)

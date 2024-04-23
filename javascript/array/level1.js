d8f04388ac30b0db79bf9c45100a3eaf694c0044
Kaylin Khanal <kyalinnop@gmail.com>
2024-04-23 09:07:04 +0545

    added tasks

---
+++ javascript/array/level1.js
@@ -0,0 +1,14 @@
+const arr = [3,5,2]
+//Q1 calculate length of array and console log
+//expected output is 3
+
+
+
+
+
+//Q2 reverse the array and console log
+//expected output [2,5,3]
+
+
+///Q3 join the array and console log
+// expected output 352
--- javascript/basic/operators.js
+++ javascript/basic/operators.js
@@ -11,5 +11,18 @@ const b = '10'
 console.log(a === b)
 
 
+
 // generate a random number between 0 and 5, if it is lesser than 3,
 //console.log "lesser than 3" else "Greater"
+// Hint Math.random
+const randomNum = Math.random()*6
+const roundOffVal = Math.floor(randomNum)
+console.log(roundOffVal)
+if(roundOffVal<3){
+    console.log(roundOffVal+ 'is less than 3')
+}else if(roundOffVal === 3){
+    console.log(roundOffVal+ 'is equals to 3')
+}
+else{
+    console.log(roundOffVal+ " is greater than 3")
+}

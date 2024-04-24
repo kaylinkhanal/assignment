6bc21912ac17cbc1e286bae5460d2b1440418329
Kaylin Khanal <kyalinnop@gmail.com>
2024-04-24 16:13:35 +0545

    added level 2 assignments

--- javascript/array/level1.js
+++ javascript/array/level1.js
@@ -16,3 +16,19 @@ const arr =  [3,5,2]
 
 
 
+//Q4 Remove the last element from the array and log the modified array to the console
+
+
+// Q5 Check if all elements in the num  are even. Log "All even" or "Not all even" accordingly.
+const num = [6,4,2,8,10,12]
+
+
+
+// Find the index of a specific element in the array. Log the index to the console. 
+//expected output: 3
+
+const allNum = [3, 5, 2, 8, 5];
+const searchElement = 2;
+
+
+
--- javascript/array/level2.js
+++ javascript/array/level2.js
@@ -1,3 +1,18 @@
 const arr =  [3,5,2]
 //console.log the maximum number of an array
 //expectedd output 5
+
+
+//Q2 Remove a specific element 2 from the array. Log the modified array to the console.
+// expected output: [3,5]
+
+const elementToRemove = 2;
+
+
+//Q3 Multiply each element of the array by a constant value 5 and log the modified array to the console.
+//expected output [15,10]
+
+
+//find intersection (common elements) of arr and arr2
+//expected output: [5]
+const arr2 = [5,21,32]

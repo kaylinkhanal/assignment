ea56d9ff3f71dc91b1c9e3e4b8d54ce600fa8ba4
Kaylin Khanal <kyalinnop@gmail.com>
2024-04-30 08:32:51 +0545

    added navbar from nextui

--- react/nextdayone/src/app/register/page.js
+++ react/nextdayone/src/app/register/page.js
@@ -4,9 +4,9 @@ const Register = () => {
     <div className="flex justify-center ">
     <div className='border border-black m-7 w-72 p-4 '>
         <Input type="email" label="Email" /> <br/>
-        <input placeholder='enter password' type='password'/> <br/>
-        <input placeholder='enter address'/> <br/>
-        <input placeholder='enter email'/> <br/>
+        <Input label="Username" /> <br/>
+        <Input  label="Address" /> <br/>
+        <Input type="password" label="Password" /> <br/>
         <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">
       Submit
     </Button>
---
+++ react/nextdayone/src/components/navbar/page.js
@@ -0,0 +1,42 @@
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+
+
+const CustomNavbar=()=> {
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <p className="font-bold text-inherit">Astro</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
+
+
+export default CustomNavbar

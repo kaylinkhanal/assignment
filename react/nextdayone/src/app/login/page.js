9308e89c3f157e741af45618839fefef24451ada
Kaylin Khanal <kyalinnop@gmail.com>
2024-04-30 08:39:17 +0545

    import navbar to login register page

--- react/nextdayone/src/app/login/page.js
+++ react/nextdayone/src/app/login/page.js
@@ -1,6 +1,10 @@
+import CustomNavbar from "@/components/navbar/page"
+
+
 const Login = () => {
   return (
     <div>
+        <CustomNavbar/>
         <input placeholder='enter password' type='password'/> <br/>
         <input placeholder='enter email'/> <br/>
         <button className="bg-black text-white m-4">Submit</button>
--- react/nextdayone/src/app/register/page.js
+++ react/nextdayone/src/app/register/page.js
@@ -1,17 +1,23 @@
+import CustomNavbar from "@/components/navbar/page";
 import {Button,Input} from "@nextui-org/react";
+
 const Register = () => {
   return (
-    <div className="flex justify-center ">
-    <div className='border border-black m-7 w-72 p-4 '>
-        <Input type="email" label="Email" /> <br/>
-        <Input label="Username" /> <br/>
-        <Input  label="Address" /> <br/>
-        <Input type="password" label="Password" /> <br/>
-        <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">
-      Submit
-    </Button>
+    <div>
+      <CustomNavbar/>
+      <div className="flex justify-center ">
+      <div className='border border-black m-7 w-72 p-4 '>
+          <Input type="email" label="Email" /> <br/>
+          <Input label="Username" /> <br/>
+          <Input  label="Address" /> <br/>
+          <Input type="password" label="Password" /> <br/>
+          <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">
+        Submit
+      </Button>
+      </div>
+      </div>
     </div>
-    </div>
+ 
   )
 }
 
